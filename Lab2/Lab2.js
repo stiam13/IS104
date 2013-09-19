6c6c98da1e351508cd68a9f9734352e00a058abb
stiam13 <stiam13@student.uia.no>
2013-09-19 16:57:22 +0200

    Lab1 og Lab 2
    Oppdatert Readme i Lab1
    Ny oppgave Lab2

---
+++ Lab2/Lab2.js
@@ -0,0 +1,7 @@
+
+
+function printToOutputBox(selectobj){
+ var e_proglangs = document.getElementById("proglangs");
+ var e_outputbox = document.getElementById("outputbox");
+ e_outputbox.innerHTML = "Du har valgt programmeringsspråket " + e_proglangs.options[selectobj.selectedIndex].text;
+}
